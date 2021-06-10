9dd54fd3c94b464bf4fd5961ae3901ff2f9a90ec
saket-07 <cse190001054@iiti.ac.in>
2021-06-11 01:10:11 +0530

    authencation setup

---
+++ botsrc.py
@@ -0,0 +1,6 @@
+from decouple import config
+import tweepy
+
+auth = tweepy.OAuthHandler(config("CONSUMER_KEY"), config("CONSUMER_SECRET"))
+auth.set_access_token(config("ACCESS_KEY"), config("ACCESS_SECRET"))
+api = tweepy.API(auth)
