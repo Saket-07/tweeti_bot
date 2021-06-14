59e223f1c19f3059e398670e96a3b0c23a148f72
rishabh-soni <rishabh.rs.001@gmail.com>
2021-06-15 03:14:06 +0530

    Added some more hashtags for retweet and increased the sleep time.

--- bird_retweet.py
+++ bird_retweet.py
@@ -3,25 +3,30 @@ import tweepy
 import time as tim
 from datetime import *
 
-
 auth = tweepy.OAuthHandler(config("CONSUMER_KEY"), config("CONSUMER_SECRET"))
 auth.set_access_token(config("ACCESS_KEY"), config("ACCESS_SECRET"))
 api = tweepy.API(auth)
 
+
 def cmp(ab):
     return ab.created_at
 
+
 def retweet():
     search_results = api.search(q="#bird #birds", count=100, lang="en")
-    search_results+=(api.search(q="#birdphotography", count=100, lang="en"))
-    search_results.sort(reverse=True, key=cmp)
+    search_results += (api.search(q="#birdphotography", count=100, lang="en"))
+    search_results += (api.search(q="#swan", count=100, lang="en"))
+    search_results += (api.search(q="#parrot", count=100, lang="en"))
+    search_results += (api.search(q="#birdwatching", count=100, lang="en"))
 
+    search_results.sort(reverse=True, key=cmp)
     api.retweet(search_results[0].id)
 
+
 bird_retweet_time = datetime.now()
+
 while True:
     while datetime.now() < bird_retweet_time:
-        tim.sleep(1)
+        tim.sleep(3600)
     retweet()
     bird_retweet_time += timedelta(hours=4)
-
